e7599f1052301e7243a28e747000a1ccbd871fc8
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2025-02-02 17:58:04 +0800

    chore: install @fullhuman/postcss-purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,17 @@
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss'
+
 export default {
-  plugins: {},
+  plugins: [
+    process.env.NODE_ENV === 'production' &&
+      purgeCSSPlugin({
+        content: [
+          './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+          './storage/framework/views/*.php',
+          './resources/**/*.blade.php',
+          './resources/**/*.{ts,tsx,js,jsx}',
+        ],
+        keyframes: true,
+        variables: true,
+      }),
+  ],
 }
